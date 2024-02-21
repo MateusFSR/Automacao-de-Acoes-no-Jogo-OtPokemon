99cfa46e260343a884b1e6e783624fe16568b3cb
Mateus F S Ribeiro <98323230+MateusFSR@users.noreply.github.com>
2024-02-21 16:00:56 -0300

    Update avnc.py

--- avnc.py
+++ avnc.py
@@ -2,8 +2,8 @@ class PokemonBotApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Pokemon Bot")
-        largura_janela = 250
-        altura_janela = 130
+        largura_janela = 350
+        altura_janela = 120
         self.root.geometry(f"{largura_janela}x{altura_janela}")
 
         # Configuração para manter a janela no topo
@@ -11,26 +11,43 @@ class PokemonBotApp:
 
         # Frame para os botões AutoBattle
         frame_verificar = tk.Frame(root)
-        frame_verificar.grid(row=0, column=0, padx=8, pady=10, sticky='w')
+        frame_verificar.grid(row=0, column=0, padx=8, pady=10, sticky='nsew')
+        
+        self.status_label = tk.Label(frame_verificar, text="Auto Battle", font=('Verdana', 8))
+        self.status_label.grid(row=0, column=0, pady=5)
 
-        self.start_verificar_button = tk.Button(frame_verificar, text="Iniciar AutoBattle", command=self.iniciar_verificar, font=('Verdana', 8))
-        self.start_verificar_button.grid(row=0, column=0, pady=5)
+        self.start_verificar_button = tk.Button(frame_verificar, text="Iniciar", command=self.iniciar_verificar, font=('Verdana', 8))
+        self.start_verificar_button.grid(row=1, column=0, pady=5)
 
-        self.stop_verificar_button = tk.Button(frame_verificar, text="Parar AutoBattle", command=self.parar_verificar, state=tk.DISABLED, font=('Verdana', 8))
-        self.stop_verificar_button.grid(row=1, column=0, pady=5)
-
-        self.dummy_button = tk.Button(frame_verificar, text="Posicao Pokemon", font=('Verdana', 8), pady=1, command=self.capturar_posicao)
-        self.dummy_button.grid(row=2, column=0, pady=5)
+        self.stop_verificar_button = tk.Button(frame_verificar, text="Parar", command=self.parar_verificar, state=tk.DISABLED, font=('Verdana', 8))
+        self.stop_verificar_button.grid(row=2, column=0, pady=5)
 
         # Frame para os botões AutoCatch
         frame_capturar = tk.Frame(root)
-        frame_capturar.grid(row=0, column=1, padx=8, pady=10)
+        frame_capturar.grid(row=0, column=1, padx=8, pady=10, sticky='nsew')
+        
+        self.status_label = tk.Label(frame_capturar, text="Auto Captura", font=('Verdana', 8))
+        self.status_label.grid(row=0, column=0, pady=5)
 
-        self.start_capturar_button = tk.Button(frame_capturar, text="Iniciar AutoCatch", command=self.iniciar_capturar, font=('Verdana', 8))
-        self.start_capturar_button.grid(row=0, column=0, pady=5)
+        self.start_capturar_button = tk.Button(frame_capturar, text="Iniciar", command=self.iniciar_capturar, font=('Verdana', 8))
+        self.start_capturar_button.grid(row=1, column=0, pady=5)
 
-        self.stop_capturar_button = tk.Button(frame_capturar, text="Parar AutoCatch", command=self.parar_capturar, state=tk.DISABLED, font=('Verdana', 8))
-        self.stop_capturar_button.grid(row=1, column=0, pady=5)
+        self.stop_capturar_button = tk.Button(frame_capturar, text="Parar", command=self.parar_capturar, state=tk.DISABLED, font=('Verdana', 8))
+        self.stop_capturar_button.grid(row=2, column=0, pady=5)
+
+        # Frame para configuraçoes
+        frame_config = tk.Frame(root)
+        frame_config.grid(row=0, column=2, padx=10, pady=10, sticky='nsew')
+
+        self.status_label = tk.Label(frame_config, text="Configurações", font=('Verdana', 8))
+        self.status_label.grid(row=0, column=0, pady=5)
+
+        self.dummy_button = tk.Button(frame_config, text="Posicao Batalha (H)", font=('Verdana', 8), pady=1, padx=7, command=self.capturar_battle)
+        self.dummy_button.grid(row=1, column=0, pady=5)  
+        
+        self.dummy_button = tk.Button(frame_config, text="Posicao Pokemon (H)", font=('Verdana', 8), pady=1, command=self.capturar_posicao)
+        self.dummy_button.grid(row=2, column=0, pady=5)
+      
 
         self.running_capturar = False
         self.running_verificar = False
@@ -60,7 +77,7 @@ class PokemonBotApp:
     def verificar_mudanca_imagem(self):
         while self.running_verificar:
             try:                     
-                battle = pa.locateOnScreen('battle.png', confidence=0.8, region=battle_region)
+                battle = pa.locateOnScreen('battle.png', confidence=0.8, region=self.capturar_tela)
                 if battle is not None:
                         print("Sem Pokemons para batalha")
                         time.sleep(0.5)
@@ -131,6 +148,16 @@ class PokemonBotApp:
         y = self.bposicao.y
         self.battle_position = (x, y)
         print(self.battle_position)
+
+    def capturar_battle(self):
+        keyboard.wait('h')
+        self.btela = pa.locateOnScreen('battle.png', confidence=0.7)
+        self.left = self.btela.left
+        self.top = self.btela.top
+        self.width = self.btela.width
+        self.height = self.btela.height
+        self.capturar_tela = (self.left, self.top, self.width, self.height)
+        print(self.capturar_tela)
 
 if __name__ == "__main__":
     root = tk.Tk()
